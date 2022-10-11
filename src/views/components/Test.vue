91acab6352df6f0c879b55a63b5ccf0f23583755
memory <zhangzhigang@pxsemic.com>
2022-10-11 13:40:38 +0800

    1

---
+++ src/views/components/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test">
+    <h2>{{ msg }}</h2>
+    <p>{{ user.name }}</p>
+    <p>{{ user.age }}</p>
+    <p>{{ user.area }}</p>
+    <p>{{ filterArr }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, reactive, ref } from 'vue';
+
+const msg = ref('消息提示');
+const user = reactive({
+  name: '张三',
+  age: 24,
+  area: '上海浦东新区',
+});
+
+const filterArr = computed(() => [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].filter((item) => item > 5));
+</script>
+
+<style scoped lang="scss"></style>
