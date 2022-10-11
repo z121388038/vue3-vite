d96c0d366ef8294202fd9006bacfe3c654a2f491
memory <zhangzhigang@pxsemic.com>
2022-10-11 13:40:38 +0800

    1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,9 +7,6 @@
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
   }
 }
 </style>
---
+++ src/views/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test">
+    <h2>{{ msg }}</h2>
+    <p>{{ user.name }}</p>
+    <p>{{ user.age }}</p>
+    <p>{{ user.area }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+const msg = ref('消息提示');
+const user = reactive({
+  name: '张三',
+  age: 24,
+  area: '上海',
+});
+</script>
+
+<style scoped lang="scss"></style>
